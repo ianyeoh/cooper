fb834cf701082959e9745e5438b16bff68838387
Ian Yeoh <ian.yeoh.st@prysmian.com>
2025-07-31 15:22:10 +1000

    style: standardise LF across linting tools for cross-platform development

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,17 @@
-import eslint from "@eslint/js";
-import tseslint from "typescript-eslint";
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
     ignores: [
-      "**/node_modules/",
-      "**/build/",
-      "**/.next/",
-      "eslint.config.mjs",
-      "**/*.js",
-      "**/tailwind.config.ts",
-      "**/next.config.mjs",
-      "**/.nyc_output/",
+      '**/node_modules/',
+      '**/build/',
+      '**/.next/',
+      'eslint.config.mjs',
+      '**/*.js',
+      '**/tailwind.config.ts',
+      '**/next.config.mjs',
+      '**/.nyc_output/',
     ],
   },
   eslint.configs.recommended,
